5a4236f6f8d5208598b98570dc16c343ad823c17
Maria Isabel <mibalbuquerque@outlook.com>
2022-12-29 22:23:32 -0300

    melhorias nas cores

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,4 +1,5 @@
 
+
 const pokeApi = {}
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
@@ -6,11 +7,11 @@ function convertPokeApiDetailToPokemon(pokeDetail) {
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const types = pokeDetail.types.map((typeSlot => typeSlot.type.name))
     const [type] = types
 
-    pokemon.types = types
     pokemon.type = type
+    pokemon.types = types
 
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
@@ -23,7 +24,8 @@ pokeApi.getPokemonDetail = (pokemon) => {
         .then(convertPokeApiDetailToPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+
+pokeApi.getPokemons = (offset = 0, limit = 4) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
@@ -31,5 +33,5 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
+        .then((pokemonDetails) => pokemonDetails)
 }
--- assets/js/pokemon-model.js
+++ assets/js/pokemon-model.js
@@ -1,8 +1,9 @@
 
-class Pokemon {
+class Pokemon{
     number;
     name;
     type;
     types = [];
     photo;
 }
+
